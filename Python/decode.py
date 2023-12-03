5ab589f36e8438aea0cdb98c70f6f16d8d60967a
Taj borthwick <sunanborthwic@gmail.com>
2023-12-03 21:30:50 +0700

    New and better Python code
    Make python code better and less complicate

--- Python/decode.py
+++ Python/decode.py
@@ -3,24 +3,11 @@ class Decode:
     def __init__(self, decode_char, data) -> None:
         self.encode_value = data 
         self.decode_char = decode_char
-        self.formatForDecode()
 
-    def formatForDecode(self, string = "", format_result = []):
-        self.encode_value = self.encode_value.split()
-        for i in self.encode_value:
-            for j, v in enumerate(i, start = 1):
-                string += v
-                if j % 3 == 0:
-                    format_result.append(string)
-                    string = ""
-        self.encode_value = format_result
-
-    def formatDecode(self, decode_value, decode_result = ""):
-        for i in decode_value:
-            decode_result += i
-        return decode_result
-
-    def decode(self, decode_value = []):
-        for i in self.encode_value:
-            decode_value.append(self.decode_char[i])
-        return self.formatDecode(decode_value)
+    def decode(self, string = "", result = ""):
+        for i, v in enumerate(self.encode_value, start = 1):
+            string += v
+            if i % 3 == 0:
+                result += self.decode_char[string]
+                string = ""
+        return result
