d5a71cfd58e07e489536edc33a9ee1bff67f418a
Taj borthwick <sunanborthwic@gmail.com>
2023-12-05 15:14:12 +0700

    New UI out now

--- Python/ui.py
+++ Python/ui.py
@@ -1,63 +1,64 @@
-import functionfile as fu
 import encode, decode
-import json, tkinter as tk
+import json, tkinter as tk, pyperclip
 
 class Ui:
 
     def __init__(self) -> None:
         self.app = tk.Tk()
-        with open("decodeandencode.json", "r") as file: data = json.load(file)
-        self.encode_decode_char = data
-        self.result = ""
+        with open("decodeandencode.json", "r") as file: en_de_data = json.load(file)
+        self.encode_decode_char = en_de_data
+        with open("uiconfig.json", "r") as file: config_data = json.load(file)
+        self.HEIGHT = config_data["config"]["HEIGHT"]
+        self.WIDTH = config_data["config"]["WIDTH"]
 
-    def getUserInputText(self):
-        self.user_input_text = self.enter_text_entry.get()
-
-    def getUserInputChoice(self):
-        self.user_input_choice = self.enter_choice_entry.get()
-
-    def processing(self): 
-        if self.user_input_choice == "encode":
-            encoding = encode.Encode(encode_char = self.encode_decode_char["encode"], data = self.user_input_text)
-            self.result = encoding.encode()
-        if self.user_input_choice == "decode":
-            decoding = decode.Decode(decode_char = self.encode_decode_char["decode"], data = self.user_input_text)
-            self.result = decoding.decode()        
+    def encode(self):
+        user_input_text = self.enter_text_entry.get()
+        encoding = encode.Encode(encode_char = self.encode_decode_char["encode"], data = user_input_text)
+        self.result = encoding.encode()
         self.result_text.config(text = self.result)
+
+    def decode(self): 
+        user_input_text = self.enter_text_entry.get()
+        decoding = decode.Decode(decode_char = self.encode_decode_char["decode"], data = user_input_text)
+        self.result = decoding.decode()        
+        self.result_text.config(text = self.result)
+
+    def copy_text(self):
+        pyperclip.copy(self.result)
 
     def configure(self):
         self.app.title("EncodeAndDecodePython")
-        self.app.geometry("500x500")
-        
-        self.ui_info_text = tk.Label(self.app, text = "This is the version 0.1 of the ui")
-        self.ui_info_text.pack()
-        self.code_info_text = tk.Label(self.app, text = "Current code version 0.25 alpha")
-        self.code_info_text.pack()
-        self.how_to_use = tk.Label(self.app, text = "If you have trouble using this UI, you can refer to the instruction.txt file\n or visit the GitHub download page (the download page for this source code)\n for more information.")
-        self.how_to_use.pack()
+        self.app.resizable(width = False, height = False)
+        self.app.geometry(f"{self.WIDTH}x{self.HEIGHT}")
+        self.center = {"x": self.WIDTH / 2, "y": self.HEIGHT / 2}
 
-        self.enter_text_text = tk.Label(self.app, text = "Enter your text here")
-        self.enter_text_text.place(x = 250, y = 150, anchor = tk.CENTER)
-        self.enter_text_entry = tk.Entry(self.app, width = 30)
-        self.enter_text_entry.place(x = 250, y = 170, anchor = tk.CENTER)
-        self.get_text_entry = tk.Button(self.app, text = "Enter", command = self.getUserInputText)
-        self.get_text_entry.place(x = 250, y = 200, anchor = tk.CENTER)
-        
-        self.choice_text_text = tk.Label(self.app, text = "Enter your choice here")
-        self.choice_text_text.place(x = 250, y = 280, anchor = tk.CENTER)
-        self.enter_choice_entry = tk.Entry(self.app, width = 30)
-        self.enter_choice_entry.place(x = 250, y = 300, anchor = tk.CENTER)     
-        self.get_choice_entry = tk.Button(self.app, text = "Enter", command = self.getUserInputChoice)
-        self.get_choice_entry.place(x = 250, y = 330, anchor = tk.CENTER)
+        if self.WIDTH >= 350 and self.HEIGHT >= 350:
+            self.ui_info_text = tk.Label(self.app, text = "This is the version 0.2 of the ui")
+            self.ui_info_text.pack()
+            self.code_info_text = tk.Label(self.app, text = "Current code version 0.3 alpha")
+            self.code_info_text.pack()
+            self.how_to_use = tk.Label(self.app, text = "If you have trouble using this UI, you can refer to the instruction.txt file or visit the GitHub download page (the download page for this source code) for more information.", wraplength = self.WIDTH - 100)
+            self.how_to_use.pack()
 
-        self.done_button = tk.Button(self.app, text = "Done", command = self.processing)
-        self.done_button.place(x = 250, y = 450, anchor = tk.CENTER)
+            self.enter_text_text = tk.Label(self.app, text = "Enter your text here")
+            self.enter_text_text.place(x = self.center["x"], y = self.center["y"] - 50, anchor = tk.CENTER)
+            self.enter_text_entry = tk.Entry(self.app, width = 30)
+            self.enter_text_entry.place(x = self.center["x"], y = self.center["y"] - 30, anchor = tk.CENTER)
+            
+            self.choice_encode_button = tk.Button(self.app, text = "Encode", command = self.encode)
+            self.choice_encode_button.place(x = self.center["x"] - 30, y = self.center["y"] - 5, anchor = tk.CENTER) 
 
-        #self.help_button = tk.Button(self.app, text = "Help")
-        #self.help_button.place(x = 30, y = 30, anchor = tk.CENTER)
+            self.choice_decode_button = tk.Button(self.app, text = "Decode", command = self.decode)
+            self.choice_decode_button.place(x = self.center["x"] + 30, y = self.center["y"] - 5, anchor = tk.CENTER)
 
-        self.result_text = tk.Label(self.app, text = "")
-        self.result_text.place(x = 250, y = 400, anchor = tk.CENTER)
+            self.result_text = tk.Label(self.app, text = "", wraplength = self.WIDTH - 50)
+            self.result_text.place(x = self.center["x"], y = self.center["y"] + 100, anchor = tk.CENTER)
+
+            self.copy_button = tk.Button(self.app, text = "Copy", command = self.copy_text)
+            self.copy_button.place(x = self.center["x"], y = self.center["y"] + 50, anchor = tk.CENTER)
+        else:
+            self.warning_text = tk.Label(self.app, text = "WIDTH AND HEIGHT NEED TO BE MORE THAN 500", wraplength = self.WIDTH - 10)
+            self.warning_text.pack()
 
     def main(self):
         self.configure()
